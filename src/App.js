7594d804a606daddbfd0c881680055e9f2e73f34
Yechun Song <yechun.song@rea-group.com>
2020-01-10 18:44:33 +0800

    basic code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.scss';
+import { Form, Input, Button, DatePicker } from 'antd';
+import moment from 'moment'
+const { RangePicker } = DatePicker;
 
-function App() {
+function App(props) {
+  const [result, setResult] = useState({ income: 0, outcome: 0 })
+  const { getFieldDecorator, getFieldsValue } = props.form
+  const { income, outcome } = result
+
+  const onCalculate = () => {
+    const { income, perdiem, hotel, ticket, date } = getFieldsValue()
+    const dateRange = ~~moment(date[1] - date[0]).format('D')
+
+    setResult({
+      income: ~~income * dateRange,
+      outcome: (~~perdiem + ~~hotel + ~~ticket) * dateRange
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className={styles.app}>
+      <h1 className={styles.title}>Business Budget Calculator</h1>
+      <div>
+        <div className={styles.bugdetForm}>
+          <article>
+            <h4>Income (per day)</h4>
+            <Form.Item>
+              {getFieldDecorator('income', {
+                rules: [{ required: true, message: 'Please input your Income!' }],
+              })(
+                <Input placeholder="Income" type="number" />
+              )}
+            </Form.Item>
+          </article>
+
+          <article>
+            <h4>Outcome (per day)</h4>
+            <div>
+              {getFieldDecorator('perdiem', {
+                rules: [{ required: true, message: 'Please input your per diem!' }],
+              })(
+                <Input placeholder="perdiem" type="number" />
+              )}
+              {getFieldDecorator('hotel', {
+                rules: [{ required: true, message: 'Please input your hotel fee per night!' }],
+              })(
+                <Input placeholder="hotel" type="number" />
+              )}
+              {getFieldDecorator('ticket', {
+                rules: [{ required: true, message: 'Please input your ticket per day!' }],
+              })(
+                <Input placeholder="ticket" type="number" />
+              )}
+            </div>
+          </article>
+
+          <article>
+            <h4>Date</h4>
+            {getFieldDecorator('date', {
+              rules: [{ required: true, message: 'Please select your start end end date!' }],
+            })(
+              <RangePicker />
+            )}
+
+          </article>
+
+          <article>
+            <Button type="primary" onClick={onCalculate}>Calculate</Button>
+          </article>
+        </div>
+
+        <article>
+          <h4>Total result</h4>
+          <div>
+            <div>Income: {income}</div>
+            <div>Outcome: {outcome}</div>
+          </div>
+        </article>
+      </div>
+    </Form>
   );
 }
 
-export default App;
+const WrappedApp = Form.create({ name: 'budgetForm' })(App);
+export default WrappedApp;
